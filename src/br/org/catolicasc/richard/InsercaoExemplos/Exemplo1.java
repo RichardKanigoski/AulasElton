91ac3daaa3421968d9ea7cb982b66132c311a65c
Richard Kanigoski <richardkanigoski@unerj.br>
2016-03-04 19:28:16 -0300

    Criado o exemplo

--- src/br/org/catolicasc/richard/InsercaoExemplos/Exemplo1.java
+++ src/br/org/catolicasc/richard/InsercaoExemplos/Exemplo1.java
@@ -1,5 +1,31 @@
 package br.org.catolicasc.richard.InsercaoExemplos;
 
-public class Exemplo1 {
+import java.util.Scanner;
 
+public class Exemplo1 {
+	public static void main(String [] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int x[] = new int[5];
+		int i, j, eleito;
+		
+		for(i = 0; i <= 4; i++) {
+			System.out.print("Digite o " + (i + 1) + " número: ");
+			x[i] = in.nextInt();
+		}
+		for(i = 1; i <= 4; i++) {
+			eleito = x[i];
+			j = i - 1;
+			
+			while( j >= 0 && x[j] > eleito) {
+				x[j + 1] = x[j];
+				j = j - 1;
+			}
+			x[j + 1] = eleito;
+		}
+		
+		for(i = 0; i <= 4; i++) {
+			System.out.println(i + 1 + " número " +  x[i]);
+		}
+	}
 }
